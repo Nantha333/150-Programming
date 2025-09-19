45a18dfed1a8b460833c2e667574fc08cfc7e47b
Nantha Kumar <fftnantha007@gmail.com>
2025-09-19 12:05:44 +0530

    commit

--- src/Basic/PrimeNumbers.java
+++ src/Basic/PrimeNumbers.java
@@ -1,5 +1,22 @@
 package Basic;
-
+import java.util.Scanner;
 public class PrimeNumbers {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        boolean prime=true;
+        for(int i=2; i<n; i++){
+            if(n%2==0){
+                prime=false;
+            }
+
+        }
+        if(prime){
+            System.out.println("Prime");
+        }else{
+            System.out.println("No Prime");
+        }
+    }
 
 }
