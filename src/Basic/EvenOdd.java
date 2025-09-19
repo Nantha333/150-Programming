972f510edbd210f701a81e67d693ee37000c9e55
Nantha Kumar <fftnantha007@gmail.com>
2025-09-19 11:58:40 +0530

    Initial commit

---
+++ src/Basic/EvenOdd.java
@@ -0,0 +1,16 @@
+package Basic;
+import java.util.Scanner;
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+            int n = in.nextInt();
+            if(n%2==0){
+                System.out.println("Even");
+            }else{
+                System.out.println("Odd");
+            }
+
+        }
+    }
+
+
